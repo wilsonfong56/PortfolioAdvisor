b2918d266ff78529d21d0f0b3fed54268178e950
wilsonfong56 <wilsonfong56@gmail.com>
2025-01-18 16:54:03 -0800

    Added login endpoint

--- backend/src/authentication/__init__.py
+++ backend/src/authentication/__init__.py
@@ -1,24 +1,42 @@
-from flask import Blueprint
-from flask import request
+from flask import Blueprint, request, jsonify
 from ...models.User import User
 from ..shared import db
+from .authentication import hash_password, verify_password
 
 authentication_routes = Blueprint('authentication_routes', __name__)
 
 @authentication_routes.route('/register', methods=['POST'])
 def register():
-    name = request.form['name']
-    email = request.form['email']
-    password = request.form['password']
+    data = request.json
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
     if not name or not email or not password:
-        return "All fields are required."
+        return jsonify({"message": "All fields are required."}), 404
 
     existing_user = User.query.filter(User.email == email).first()
     if existing_user:
-        return "Email taken."
+        return jsonify({"message": "Email taken."}), 404
 
-    new_user = User(name=name, email=email, password=password)
+    new_user = User(name=name, email=email, password_hash=hash_password(password))
     db.session.add(new_user)
     db.session.commit()
-    return "Registration Successful!"
+    return jsonify({"message": "Registration Successful!"}), 200
 
+@authentication_routes.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({"message": "All fields are required."}), 404
+
+    existing_user = User.query.filter(User.email == email).first()
+
+    if existing_user:
+        correct_hashed_password = existing_user.password_hash
+        if verify_password(password, correct_hashed_password):
+            return jsonify({"message": "Successful login"}), 200
+
+    return jsonify({"message": "Unsuccessful login"}), 404
