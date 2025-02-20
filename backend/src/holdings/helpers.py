6274f1f41b0dca32a736beff1beef44d9d2c9d7f
wilsonfong56 <wilsonfong56@gmail.com>
2025-02-20 10:47:52 -0800

    Added functions to get industry and insider transactions of a stock

--- backend/src/holdings/helpers.py
+++ backend/src/holdings/helpers.py
@@ -8,5 +8,12 @@ finnhub_client = finnhub.Client(api_key=os.getenv("FINNHUB_API_KEY"))
 def getPeers(symbol):
     return finnhub_client.company_peers(symbol)
 
+def getIndustry(symbol):
+    return finnhub_client.company_profile2(symbol=symbol)
+
+def getInsiderTransactions(symbol):
+    return finnhub_client.stock_insider_transactions(symbol, "2025-01-01")
+
 if __name__ == "__main__":
-    print(getPeers("AAPL"))
+    print(getPeers("TEM"))
+    print(getIndustry("TECH"))
